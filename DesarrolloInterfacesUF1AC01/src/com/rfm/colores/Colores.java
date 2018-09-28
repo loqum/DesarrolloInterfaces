b72e486d152547edd8b20191e23cd5f3cedb64b8
loqum <ruben693@gmail.com>
2018-09-28 11:57:50 +0200

    Añadida funcionalidad de aplicación de color y de reseteo en botones

--- DesarrolloInterfacesUF1AC01/src/com/rfm/colores/Colores.java
+++ DesarrolloInterfacesUF1AC01/src/com/rfm/colores/Colores.java
@@ -7,9 +7,15 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.BoxLayout;
 import javax.swing.border.BevelBorder;
-import java.awt.Color;
+
+import com.rfm.utils.Utils;
+
 import java.awt.SystemColor;
 import java.awt.Component;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Colores {
 
@@ -41,7 +47,7 @@ public class Colores {
 		frameColores.getContentPane().setBackground(SystemColor.menu);
 		frameColores.setResizable(false);
 		frameColores.setTitle("Colores");
-		frameColores.setBounds(100, 100, 450, 400);
+		frameColores.setBounds(100, 100, 450, 450);
 		frameColores.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameColores.getContentPane().setLayout(null);
 
@@ -67,15 +73,48 @@ public class Colores {
 		frameColores.getContentPane().add(panelPosition);
 		panelPosition.setLayout(new BoxLayout(panelPosition, BoxLayout.X_AXIS));
 
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Abajo");
-		panelPosition.add(rdbtnNewRadioButton_1);
+		JRadioButton radioButtonDown = new JRadioButton("Abajo");
+		panelPosition.add(radioButtonDown);
 
 		JRadioButton radioButtonUp = new JRadioButton("Arriba");
 		panelPosition.add(radioButtonUp);
-
+		
 		JPanel panelCanvas = new JPanel();
-		panelCanvas.setBounds(248, 25, 160, 143);
+		panelCanvas.setBounds(234, 25, 178, 143);
 		frameColores.getContentPane().add(panelCanvas);
+		panelCanvas.setLayout(null);
+		
+		Canvas canvas = new Canvas();
+		canvas.setBounds(0, 0, 178, 143);
+		panelCanvas.add(canvas);
+		
+		
+		radioButtonBlue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				canvas.setBackground(Color.BLUE);
+//				radioButtonRed.setSelected(false);
+//				radioButtonYellow.setSelected(false);
+				Utils.reset(radioButtonRed, radioButtonYellow);
+				
+			}
+		});
+		
+		radioButtonRed.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				canvas.setBackground(Color.RED);
+//				radioButtonBlue.setSelected(false);
+//				radioButtonYellow.setSelected(false);
+				Utils.reset(radioButtonBlue, radioButtonYellow);
+			}
+		});
+		
+		radioButtonYellow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				canvas.setBackground(Color.YELLOW);
+//				radioButtonRed.setSelected(false);
+//				radioButtonBlue.setSelected(false);
+				Utils.reset(radioButtonRed, radioButtonBlue);
+			}
+		});
 	}
-
 }
---
+++ DesarrolloInterfacesUF1AC01/src/com/rfm/utils/Utils.java
@@ -0,0 +1,12 @@
+package com.rfm.utils;
+
+import javax.swing.JRadioButton;
+
+public class Utils {
+
+	public static void reset(JRadioButton button1, JRadioButton button2) {
+		button1.setSelected(false);
+		button2.setSelected(false);
+	}
+
+}
