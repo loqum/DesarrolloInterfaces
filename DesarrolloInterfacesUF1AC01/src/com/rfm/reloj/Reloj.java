0169282b8e5c48a72fb1546d7ec8435cfc612df2
loqum <ruben693@gmail.com>
2018-10-01 17:13:36 +0200

    Creada aplicación clase Reloj

---
+++ DesarrolloInterfacesUF1AC01/src/com/rfm/reloj/Reloj.java
@@ -0,0 +1,43 @@
+package com.rfm.reloj;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Reloj {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Reloj window = new Reloj();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Reloj() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
