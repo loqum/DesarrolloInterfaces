f3402a540a7f04d80d206623352e81827691ea09
loqum <ruben693@gmail.com>
2018-10-02 15:14:46 +0200

    Cambiados iconos y añadida ventana emergente de error

--- DesarrolloInterfacesUF1AC01/src/com/rfm/utils/Utils.java
+++ DesarrolloInterfacesUF1AC01/src/com/rfm/utils/Utils.java
@@ -13,7 +13,6 @@ public class Utils {
 	private static String PATTERN = "^(\\d*\\.)?\\d+$";
 	private static String PATTERN_DECIMAL = "%.2f";
 
-
 	public static boolean isSelectedButton(JRadioButton button1, JRadioButton button2, JRadioButton button3) {
 		if (button1.isSelected() == false && button2.isSelected() == false && button3.isSelected() == false) {
 			return false;
@@ -56,31 +55,31 @@ public class Utils {
 	}
 
 	public static String centigradeToFahrenheit(double centigrade) {
-		double result = (centigrade * 1.8) + 32;		
+		double result = (centigrade * 1.8) + 32;
 		return String.valueOf(String.format(PATTERN_DECIMAL, result));
 	}
-	
+
 	public static String setFormatTime(Calendar calendar) {
 		String timeToString = null;
-		
+
 		Date currentTime = new Date();
-		
+
 		calendar.setTime(currentTime);
-		
+
 		timeToString = String.format("%1$tH:%1$tM:%1$tS", calendar);
-		
+
 		return timeToString;
 	}
-	
+
 	public static String setFormatDate(Calendar calendar) {
 		String dateToString = null;
-		
+
 		Date currentDate = new Date();
-		
+
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		dateToString = simpleDateFormat.format(currentDate);
-						
+
 		return dateToString;
 	}
 
